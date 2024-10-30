42d68b6166f93b90db682dc9ad25026802820624
sonersg <sonersg@outlook.com>
2024-10-30 09:31:43 +0300

    typedef and using

---
+++ typedef-and-using/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+// using namespace std;
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+// typedef std::string text_t;
+using text_t = std::string;
+
+int main(){
+
+    text_t name = "Soner";
+    
+    std::cout << name << "\n";
+
+    return 0;
+}
